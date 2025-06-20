9a84d93519fee567f6a0c821b34ae415aa3e0177
Cauã Pires <caua.soares@sefaz.pb.gov.br>
2025-06-20 09:35:24 -0300

    Montando estrutura da página
    Adicionando estrutura da página "Diário"; adição da páginas nas rotas da navegação

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,14 @@ import "./styles.css";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from './pages/home';
 import Libary from './pages/libary';
+import Daily from './pages/daily';
 createRoot(document.getElementById('root')!).render(
    <Router>
     <StrictMode>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/libary' element={<Libary/>}/>
+        <Route path='/daily' element={<Daily/>}/>
       </Routes>
     </StrictMode>
   </Router>
