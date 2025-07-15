040bd02a3a46dbca241804fc916c628610e2eb61
Cauã Soares <caua.soares@sefaz.pb.gov.br>
2025-07-15 20:30:03 -0300

    Testando navegabilidade

--- tests/navbar.spec.tsx
+++ tests/navbar.spec.tsx
@@ -1,22 +1,24 @@
 import { test, expect } from '@playwright/test';
 
 test('navbar functionality', async ({ page }) => {
+  //acessando aplicação
   await page.goto('http://localhost:5173/');
 
-  // Verifica se a navbar está visível
+  //verificando navbar e navegando até a seção de biblioteca
   await expect(page.locator('#navbar')).toBeVisible();
   await expect(page.locator('#navbar')).toContainText(['Biblioteca']);
 
   await page.locator('#navbar >> text=Biblioteca').click();
 
-  const libaryCard = page.locator("#navbar >> text=Biblioteca")
+  const libaryCard = page.locator("#cuidados-stoma")
 
-  await expect(libaryCard).toHaveText('Possui 0 acessos');
+  await expect(libaryCard).toContainText('Possui 0 acessos');
 
+  //executando oito cliques no card selecionado
   for (let i = 0; i < 8; i++) {
     await libaryCard.click();
   }
 
-  // Verifica se o contador foi atualizado corretamente
-  await expect(libaryCard).toHaveText('Possui 8 acessos');
+  //verificando se ação foi refletida
+  await expect(libaryCard).toContainText('Possui 8 acessos');
 });
