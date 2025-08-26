1d98b6357fadd6d39b5b81372ddfbe9c5268e955
AlvesLariany <info.larianyalves@gmail.com>
2025-08-26 11:35:57 -0300

    Criação de tipos e inclusão do json-server

---
+++ src/types/types.ts
@@ -0,0 +1,16 @@
+export type Product = {
+  id: string;
+  name: string;
+  category: string;
+  image: string;
+  description: string;
+  price: string;
+  button: string;
+};
+
+ export type Doctor = {
+    id:string;
+    name: string,
+    email: string,
+    observation: string
+}
